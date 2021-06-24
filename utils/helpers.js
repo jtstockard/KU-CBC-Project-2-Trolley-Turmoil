aaae4825612e65cffecb24ed8da6fb403a16495b
Edward Reed <relentlessreed@gmail.com>
2021-06-24 16:58:15 -0500

    Added Email input to register form

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
 module.exports = {
   format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+    return `${new Date(date).getMonth() + 1}/${new Date(
       date
-    ).getFullYear()}`;
+    ).getDate()}/${new Date(date).getFullYear()}`;
   },
   format_plural: (word, amount) => {
     if (amount !== 1) {
