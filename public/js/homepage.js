43adbd7c18c2da8ca0dcdb70f87e0cdc26917b2d
Grant Shell <grantshell33@icloud.com>
2021-06-25 13:33:07 -0500

    added answer.js, added answer model and fetched random question on homepage when logged in

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,15 @@
 console.log("Test")
 
-fetch("/api/questions")
+fetch("/api/questions/random-answerable")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data);
+    showQuestion(data);
+  });
 
-  
+function showQuestion(question){
+  questionElement = document.getElementById('answerable-question-choice-1');
+  questionElement.innerHTML = question.first_choice;
+  questionElement = document.getElementById('answerable-question-choice-2');
+  questionElement.innerHTML = question.second_choice;
+};
