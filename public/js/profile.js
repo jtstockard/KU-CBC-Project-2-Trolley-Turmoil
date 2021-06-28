1e146f14e002be8a8fb6eb5de603aa6b42801200
Grant Shell <grantshell33@icloud.com>
2021-06-28 17:03:43 -0500

    added funct to questions added by users

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,29 +2,41 @@ const questionForm = document.getElementById('question-form');
 
 questionForm.addEventListener('submit', async (e) => {
   e.preventDefault();
-  if (e.target.hasAttribute('user-id')) {
-    const id = e.target.getAttribute('user-id');
+  console.log('form submitted');
 
-    const newFirstChoice = document.getElementById('#firstChoice-register');
-    const newSecondChoice = document.getElementById('#secondChoice-register');
+  const newFirstChoice = document.getElementById('firstChoice-register').value;
+  const newSecondChoice = document.getElementById('secondChoice-register').value;
 
-    if (newFirstChoice && newSecondChoice) {
-      const response = await fetch(`/api/question-routes`, {
-        method: 'POST',
-        body: JSON.stringify({ newFirstChoice, newSecondChoice }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        document.questions.push('/');
-      } else {
-        alert('Failed to create question');
-      }
-    }
+  if (newFirstChoice && newSecondChoice) {
+    const response = await fetch(`/api/questions`, {
+      method: 'POST',
+      body: JSON.stringify({ first_choice: newFirstChoice, second_choice: newSecondChoice }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json())
+    .then(data => {
+      // [TODO] Add error handling
+      appendNewQuestionToList(data);
+      resetForm();
+      alert("Question created");
+    });;
+  }else{
+    alert("You need to fill out both choices.");
   }
 });
+
+function resetForm(){
+  document.getElementById('firstChoice-register').value = null;
+  document.getElementById('secondChoice-register').value = null;
+}
+
+function appendNewQuestionToList(question){
+  questionList = document.getElementById('user-created-questions');
+  newQuestion = document.createElement('li');
+  newQuestion.append(question.first_choice + " VS " + question.second_choice);
+  questionList.append(newQuestion);
+}
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('user-id')) {
@@ -42,8 +54,8 @@ const delButtonHandler = async (event) => {
   }
 };
 
-document
-  .querySelector('.question-form')
-  .addEventListener('submit', newFirstChoice, newSecondChoice);
+// document
+//   .querySelector('.question-form')
+//   .addEventListener('submit', newFirstChoice, newSecondChoice);
 
-document.querySelector('.user-id').addEventListener('click', delButtonHandler);
+// document.querySelector('.user-id').addEventListener('click', delButtonHandler);
